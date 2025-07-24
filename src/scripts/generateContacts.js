c58dcc768a10eba10337a4181ba0440136be4d95
Denys Tkachenko <sinedtex@gmail.com>
2025-07-24 12:02:53 +0300

    finish tasks

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    for (let index = 0; index < number; index++) {
+        contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+};
 
 generateContacts(5);
